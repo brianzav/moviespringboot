d766ae85f68da2cdd5456755f4cc6ab79f7ee4e2
BRIAN BILLY ZAVALA CULQUE <brian.bzculque@senacsp.edu.br>
2024-05-27 17:35:28 -0300

    Adding project structure

---
+++ src/main/java/com/br/senac/dw/filmregistration/FilmregistrationApplication.java
@@ -0,0 +1,13 @@
+package com.br.senac.dw.filmregistration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmregistrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilmregistrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/senac/dw/filmregistration/FilmregistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.senac.dw.filmregistration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilmregistrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
